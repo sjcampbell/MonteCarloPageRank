68a19ca9ada54df23449f388e966e40549de1660
Sam Campbell <sjcampbell40@gmail.com>
2016-02-23 20:13:09 -0500

    Implemented Q1

--- src/main/scala/ca/uwaterloo/cs/bigdata2016w/sjcampbell/assignment2/ComputeBigramRelativeFrequencyPairs.scala
+++ src/main/scala/ca/uwaterloo/cs/bigdata2016w/sjcampbell/assignment2/ComputeBigramRelativeFrequencyPairs.scala
@@ -10,75 +10,75 @@ import org.apache.spark.Partitioner
 import org.rogach.scallop._
 
 object ComputeBigramRelativeFrequencyPairs extends Tokenizer {
-  val log = Logger.getLogger(getClass().getName())
+	val log = Logger.getLogger(getClass().getName())
 
-  // Partition based on first word in key of (String, String) 
-  class FirstWordPartitioner(numParts: Int) extends Partitioner {
-    def numPartitions: Int = numParts
+			// Partition based on first word in key of (String, String) 
+			class FirstWordPartitioner(numParts: Int) extends Partitioner {
+		def numPartitions: Int = numParts
 
-    def getPartition(key: Any): Int = {
-      val pair = key.asInstanceOf[(String, String)]
-      (pair._1.hashCode() & Int.MaxValue) % numParts
-    }
-  }
-  
-  def partitionMapper(pairCounts : Iterator[((String, String), Int)]) : Iterator[((String, String), Float)] = {
-    var sum = 0F
-    var res = List[((String, String), Int)]()
-    
-    // Could this use a map?
-    pairCounts.map(pairCount => {
-      if (pairCount._1.asInstanceOf[(String, String)]._2 == "*"){
-        sum = pairCount._2
-        (pairCount._1, sum)
-      }
-      else {
-        if (sum == 0) { println("ERROR: Divide by zero imminent!") }
-        
-        (pairCount._1, pairCount._2 / sum)
-      }
-    })
-  }
-  
-  def main(argv: Array[String]) {
-    val args = new Conf(argv)
+				def getPartition(key: Any): Int = {
+						val pair = key.asInstanceOf[(String, String)]
+								(pair._1.hashCode() & Int.MaxValue) % numParts
+        }
+	}
 
-    log.info("Input: " + args.input())
-    log.info("Output: " + args.output())
-    log.info("Number of reducers: " + args.reducers())
+	def partitionMapper(pairCounts : Iterator[((String, String), Int)]) : Iterator[((String, String), Float)] = {
+			var sum = 0F
+					var res = List[((String, String), Int)]()
 
-    val conf = new SparkConf().setAppName("Bigram Relative Frequency - Pairs")
-    val sc = new SparkContext(conf)
-    sc.setJobDescription("Computing bigram relative frequency using pairs")
+					// Could this use a map?
+					pairCounts.map(pairCount => {
+						if (pairCount._1.asInstanceOf[(String, String)]._2 == "*"){
+							sum = pairCount._2
+									(pairCount._1, sum)
+						}
+						else {
+							if (sum == 0) { println("ERROR: Divide by zero imminent!") }
 
-    val outputDir = new Path(args.output())
-    FileSystem.get(sc.hadoopConfiguration).delete(outputDir, true)
+							(pairCount._1, pairCount._2 / sum)
+						}
+					})
+	}
 
-    val textFile = sc.textFile(args.input())
-    textFile.flatMap (line => {
-      val tokens = tokenize(line)
-      if (tokens.length > 1) {
-        tokens.sliding(2).flatMap(p => 
-          List(
-                ((p(0), "*"), 1),
-                ((p(0), p(1)), 1)
-              )
-          )
-      }
-      else List()
-    })
-    // RDD[(String, String), Int]  Next: Combine
-    .reduceByKey(new FirstWordPartitioner(args.reducers()), _ + _)
-    
-    // RDD[(String, String), Int] Next: Shuffle/sort
-    .repartitionAndSortWithinPartitions(new FirstWordPartitioner(args.reducers()))
+	def main(argv: Array[String]) {
+		val args = new Conf(argv)
 
-    // RDD[(String, String, Int] Next: Calculate relative frequency 
-    .mapPartitions(partitionMapper, true) 
+				log.info("Input: " + args.input())
+				log.info("Output: " + args.output())
+				log.info("Number of reducers: " + args.reducers())
 
-    // Returns RDD[((String, String), Double)]
-    .saveAsTextFile(args.output())
-    
-    println("!!! Job Completed !!!")
-  }
+				val conf = new SparkConf().setAppName("Bigram Relative Frequency - Pairs")
+				val sc = new SparkContext(conf)
+				sc.setJobDescription("Computing bigram relative frequency using pairs")
+
+				val outputDir = new Path(args.output())
+				FileSystem.get(sc.hadoopConfiguration).delete(outputDir, true)
+
+				val textFile = sc.textFile(args.input())
+				textFile.flatMap (line => {
+					val tokens = tokenize(line)
+							if (tokens.length > 1) {
+								tokens.sliding(2).flatMap(p => 
+								List(
+										((p(0), "*"), 1),
+										((p(0), p(1)), 1)
+										)
+										)
+							}
+							else List()
+				})
+				// RDD[(String, String), Int]  Next: Combine
+				.reduceByKey(new FirstWordPartitioner(args.reducers()), _ + _)
+
+				// RDD[(String, String), Int] Next: Shuffle/sort
+				.repartitionAndSortWithinPartitions(new FirstWordPartitioner(args.reducers()))
+
+				// RDD[(String, String, Int] Next: Calculate relative frequency 
+				.mapPartitions(partitionMapper, true) 
+
+				// Returns RDD[((String, String), Double)]
+				.saveAsTextFile(args.output())
+
+				println("!!! Job Completed !!!")
+	}
 }
--- src/main/scala/ca/uwaterloo/cs/bigdata2016w/sjcampbell/assignment5/Q1.scala
+++ src/main/scala/ca/uwaterloo/cs/bigdata2016w/sjcampbell/assignment5/Q1.scala
@@ -1,10 +1,46 @@
-package ca.uwaterloo.cs.bigdata2016w.sjcampbell
+package ca.uwaterloo.cs.bigdata2016w.sjcampbell.assignment5
+
+import org.apache.log4j._
+import org.apache.hadoop.fs._
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
+import org.rogach.scallop._
 
 /*
  * How many items were shipped on a particular date?
  * select count(*) from lineitem where l_shipdate = 'YYYY-MM-DD';
+ * 
+ * Terminal alternative
+ * cat input/lineitem.tbl | awk -F'|' '{print $11}' | grep '1996-01-01' | wc -l
  */
-
-object Q1 {
-  
+object Q1 extends {
+    val log = Logger.getLogger(getClass().getName())
+    
+    def main(argv: Array[String]) {
+        val args = new Conf(argv)
+        log.info("Input: " + args.input())
+        log.info("Date: " + args.date())
+        log.info("Executors: " + args.numExecutors())
+        
+        val conf = new SparkConf().setAppName("Q1 - Count shipped items")
+        val sc = new SparkContext(conf)
+        sc.setJobDescription("Count how many items where shipped on a particular date.")
+        
+        val outputDir = new Path("q1-output")
+        FileSystem.get(sc.hadoopConfiguration).delete(outputDir, true)
+        
+        var itemCount = sc.accumulator(0)
+        val lineItems = sc.textFile(args.input() + "/lineitem.tbl")
+        val date = args.date()
+        
+        lineItems.map(line => {
+            val shipdate = line.split("\\|")(10)
+            if (shipdate.startsWith(date)) {
+                itemCount += 1
+            }
+        })
+        
+        println("ANSWER=" + itemCount)
+    }
 }
