31866dcb2996916303cc3c27ddc1d99558b6b2fe
Sam Campbell <sjcampbell40@gmail.com>
2016-03-19 18:42:45 -0400

    Began shuffled classifier

--- src/main/scala/ca/uwaterloo/cs/bigdata2016w/sjcampbell/assignment6/LineParser.scala
+++ src/main/scala/ca/uwaterloo/cs/bigdata2016w/sjcampbell/assignment6/LineParser.scala
@@ -1,6 +1,8 @@
 package ca.uwaterloo.cs.bigdata2016w.sjcampbell.assignment6
 
 class LineParser {
+    val random = scala.util.Random
+    
     def parseDataLine(line: String) : (String, String, Array[Int]) = {
         val split = line.split(" ")
         val docid = split(0)
@@ -9,6 +11,26 @@ class LineParser {
         val features = split.drop(2).map(f => f.toInt)
         
         (docid, isSpam, features)
+    }
+    
+    def parseDataLineOneKey(line: String): (Int, (String, Double, Array[Int])) = {
+        val split = line.split(" ")
+        val docid = split(0)
+        val isSpam = if (split(1) == "spam") 1.0 else 0.0
+        
+        val features = split.drop(2).map(f => f.toInt)
+        
+        (0, (docid, isSpam, features))
+    }
+    
+    def parseDataLineRandomKey(line: String): (Int, (String, Double, Array[Int])) = {
+        val split = line.split(" ")
+        val docid = split(0)
+        val isSpam = if (split(1) == "spam") 1.0 else 0.0
+        
+        val features = split.drop(2).map(f => f.toInt)
+        
+        (random.nextInt, (docid, isSpam, features))
     }
     
     def parseModelLine(line: String) : (Int, Double) = {
